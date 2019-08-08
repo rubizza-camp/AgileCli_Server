c527fe410dcdfe34d7e2e566fbb70a62c04a052d
Polina Matskevich <polina199925@gmail.com>
2019-08-08 13:26:26 +0300

    Correct spec

--- server/spec/controllers/welcome_controller_spec.rb
+++ server/spec/controllers/welcome_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe WelcomeController, type: :controller do
-  it "returns a 209 custom status code" do
+  it "returns a 200 custom status code" do
     get :index
     expect(response).to have_http_status(200)
   end
