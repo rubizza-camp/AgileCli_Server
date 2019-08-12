0a06573493428a8dd56fe40c432f6616bb0c00e3
Polina Matskevich <polina199925@gmail.com>
2019-08-12 23:02:27 +0300

    Add spec

--- server/spec/models/project_spec.rb
+++ server/spec/models/project_spec.rb
@@ -1,1 +1,11 @@
 require "rails_helper"
+
+RSpec.describe Project, type: :model do
+  before(:each) do
+    @project = Project.new
+  end
+
+  it "should be valid" do
+    @project.should be_valid
+  end
+end
