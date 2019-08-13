382d313ff829fa5222a7f858c0dbbf462c282369
Polina Matskevich <polina199925@gmail.com>
2019-08-13 17:27:38 +0300

    Fix hound

--- app/serializers/api/v1/base_serializer.rb
+++ app/serializers/api/v1/base_serializer.rb
@@ -2,7 +2,7 @@ class Api::V1::BaseSerializer < ActiveModel::Serializer
   def created_at
     object.created_at.in_time_zone.iso8601 if object.created_at
   end
-  
+
   def updated_at
     object.updated_at.in_time_zone.iso8601 if object.created_at
   end
--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::UserSerializer < Api::V1::BaseSerializer
-    attributes :id, :name,  :github_login
+  attributes :id, :name,  :github_login
 end
