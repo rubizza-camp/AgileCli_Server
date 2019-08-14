ee26151c6b38c94918694b6bac001eceed364d4e
Polina Matskevich <polina199925@gmail.com>
2019-08-14 16:14:08 +0300

    Fix hound

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::ProjectsController < Api::V1::BaseController
-    def index
-      render(json: Project.all)
-    end
-  
-    def show
-      project = Project.find_by(params[:name])
-      render(json: Api::V1::UserSerializer.new(project).to_json)
-    end
+  def index
+    render(json: Project.all)
   end
+  
+  def show
+    project = Project.find_by(params[:name])
+    render(json: Api::V1::UserSerializer.new(project).to_json)
+  end
+end
   
