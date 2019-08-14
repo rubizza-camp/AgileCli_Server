446313d005e3931d60006d9bfd55c457ee1c0524
Polina Matskevich <polina199925@gmail.com>
2019-08-14 15:18:17 +0300

    Fix config

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   def index
     render(json: User.all)
   end
-  
+
   def show
     user = User.find_by(params[:github_login])
     render(json: Api::V1::UserSerializer.new(user).to_json)
