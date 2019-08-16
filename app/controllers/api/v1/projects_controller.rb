f104fa51a0c3c5efeb33388c7a093a96b4ad00de
epfeen <epfeen@gmail.com>
2019-08-16 21:44:10 +0300

    REFACTORING CODE BY NIKITA

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,26 +1,43 @@
 class Api::V1::ProjectsController < Api::V1::BaseController
+  before_action :fetch_project, only: [:show, :destroy, :update]
+
   def index
     render(json: Project.all)
   end
 
   def show
-    @project = Project.find_by(name: params[:id])
     @project ? render(json: Api::V1::ProjectSerializer.new(@project).to_json) : create
   end
 
    # GET method
-  def new
-    @project = Project.new
-  end
+  # def new
+  #   @project = Project.new
+  # end
 
   # POST method
   def create
-    @project = Project.new(name: params[:id])
+    @project = Project.new(name: params[:name])
     render(json: Api::V1::ProjectSerializer.new(@project).to_json) if @project.save
   end
 
   def destroy
-    @project = Project.find_by(name: params[:id])
+
     @project.destroy
   end
+
+  def update
+    @project.update_attributes(name: new_name)
+  end
+
+  private
+
+  def fetch_project
+    
+    @project = Project.find_by(name: params[:name])
+  end
+
+  def project_params
+    params.require(:project).permit(:id, :name, :new_name)
+  end
+
 end
