e5b1bb0bfc5514fcce9201a01270ca3c7cc2f140
Andrew Berendeev <soligorsk162@gmail.com>
2019-08-20 16:39:47 +0300

    conflict

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
-
+Rails.application.config.assets.precompile += %w[homepage.css]
+Rails.application.config.assets.precompile += %w[welcome.css]
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
