624eb4d1eefad4990a5c0b26ea3d6a4bb4a89ad5
Andrew Berendeev <soligorsk162@gmail.com>
2019-08-21 17:15:52 +0300

    fix scss file errors

--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -1,12 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe HomepageController, type: :controller do
-
   describe "GET #homepage" do
     it "returns http success" do
       get :homepage
       expect(response).to have_http_status(:success)
     end
   end
-
 end
--- spec/controllers/manifesto_controller_spec.rb
+++ spec/controllers/manifesto_controller_spec.rb
@@ -1,12 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe ManifestoController, type: :controller do
-
   describe "GET #agile" do
     it "returns http success" do
       get :agile
       expect(response).to have_http_status(:success)
     end
   end
-
 end
