46cdd2b3a1d563e1401c4426e9b148130ca8c7d3
mirterious <inudad@yandex.ru>
2019-08-22 09:52:26 +0300

    Delete unnessesary test for welcome controller

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,8 +1,5 @@
 require "rails_helper"
 
 RSpec.describe WelcomeController, type: :controller do
-  it "returns a 200 custom status code" do
-    get :index
-    expect(true).to eq(true)
-  end
+  
 end
