8db711724a6ebd20fb38e3530a9f534868e492d1
mirterious <inudad@yandex.ru>
2019-08-22 16:41:49 +0300

    Refactor dictionary and add font Futura

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ CLIENT_SECRET = ENV["CLIENT_SECRET"]
 
 module Server
   class Application < Rails::Application
+    config.assets.enabled = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
