b8993e6fb88f9d866ae02a65ca082d1b20f375ec
Andrew Berendeev <soligorsk162@gmail.com>
2019-08-23 11:11:42 +0300

    Add login page and start page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += %w[homepage.css]
 Rails.application.config.assets.precompile += %w[manifesto.css]
 Rails.application.config.assets.precompile += %w[dictionary.css]
 Rails.application.config.assets.precompile += %w[start.css]
+Rails.application.config.assets.precompile += %w[login.css]
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
