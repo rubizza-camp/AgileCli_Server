d3f84525b6af1e8049e88dbe56157067e0d99d64
mirterious <inudad@yandex.ru>
2019-08-27 17:29:56 +0300

    Add styles for welcome mailer

--- spec/mailers/previews/invite_mailer_preview.rb
+++ spec/mailers/previews/invite_mailer_preview.rb
@@ -1,3 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invite_mailer
 class InviteMailerPreview < ActionMailer::Preview
+  def sample_mail_preview
+    InviteMailer.sample_email(User.first)
+  end
 end
