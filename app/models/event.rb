72e70c063c667ed06b83e7b11bba6cbc9432d45c
epfeen <epfeen@gmail.com>
2019-08-28 10:21:42 +0300

    frequency new param

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   belongs_to :project
 
   enum event_type: { sprint: 0, scrum: 1, retro: 2, planning: 3, review: 4 }
-  enum frequency: { daily: 0, weekly: 1, monthly: 2 }
+  enum frequency: { daily: 0, weekly: 1, monthly: 2, not_regular: 3 }
 end
