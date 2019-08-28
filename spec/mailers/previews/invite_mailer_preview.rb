f7e613692d637af933849d2864ef593cdb6724a8
mirterious <inudad@yandex.ru>
2019-08-28 12:00:40 +0300

    Delete mailers preview

--- spec/mailers/previews/invite_mailer_preview.rb
+++ spec/mailers/previews/invite_mailer_preview.rb
@@ -1,6 +1,3 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invite_mailer
 class InviteMailerPreview < ActionMailer::Preview
-  def sample_mail_preview
-    InviteMailer.sample_email(User.first)
-  end
 end
--- spec/mailers/previews/welcome_mailer_preview.rb
+++ spec/mailers/previews/welcome_mailer_preview.rb
@@ -1,6 +1,3 @@
 # Preview all emails at http://localhost:3000/rails/mailers/welcome_mailer
 class WelcomeMailerPreview < ActionMailer::Preview
-  def sample_mail_preview
-    WelcomeMailer.sample_email(User.first)
-  end
 end
