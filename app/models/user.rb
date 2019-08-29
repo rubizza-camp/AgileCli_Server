847e458ae0e43c75c7ddfd4f3e07713b7be7f950
Polina Matskevich <polina199925@gmail.com>
2019-08-29 09:37:54 +0300

    Roles to create project

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,4 @@ class User < ApplicationRecord
   has_many :userproject
   has_many :projects, through: :userproject
   has_many :tickets
-
-  after_create :assign_default_role
-
-  def assign_default_role
-    self.add_role(:team_member) if self.roles.blank?
-  end
 end
