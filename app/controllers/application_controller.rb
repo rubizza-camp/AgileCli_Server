a80c3f6678fca66122a89b8f3325f3f6f2e1584a
Andrew Berendeev <soligorsk162@gmail.com>
2019-08-29 18:02:55 +0300

    Refactor application controller session logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   def current_user
-    # id = session[:user_id]
-    @current_user = session[:user_id] if session[:user_id]
+    id = session[:user_id]
+    @current_user ||= User.find(id) if id
   end
 end
