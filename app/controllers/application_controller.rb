e02d8975f93f4f384e78e34090335c6f37d6f566
Andrew Berendeev <soligorsk162@gmail.com>
2019-08-29 17:30:14 +0300

    Fix hound errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
+    id = session[:user_id]
+    if id
+      @current_user ||= User.find(id)
     else
       @current_user = nil
     end
