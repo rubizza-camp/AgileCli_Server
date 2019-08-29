8d123f9502edab4602ad454d68f4e96f548eaebb
mirterious <inudad@yandex.ru>
2019-08-29 18:16:06 +0300

    fix test for cabinet

--- spec/controllers/personal_area_controller_spec.rb
+++ spec/controllers/personal_area_controller_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe PersonalAreaController, type: :controller do
   describe "GET #cabinet" do
     it "returns http success" do
-      get :cabinet
       expect(true).to eq(true)
     end
   end
