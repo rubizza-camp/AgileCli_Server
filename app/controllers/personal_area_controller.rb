772efcf78700d4158abdc6cbd16288e7774b5c5b
Andrew Berendeev <soligorsk162@gmail.com>
2019-08-30 13:33:27 +0300

    Fix hound error

--- app/controllers/personal_area_controller.rb
+++ app/controllers/personal_area_controller.rb
@@ -1,10 +1,8 @@
 class PersonalAreaController < ApplicationController
   def cabinet
-    if session[:user_id] == nil
-      redirect_to '/'
-    else
-      @current_user = User.find(session[:user_id])
-      @projects = @current_user.projects
-    end
+    @current_user = User.find(session[:user_id])
+    @projects = @current_user.projects
+  rescue ActiveRecord::RecordNotFound
+    redirect_to "/"
   end
 end
