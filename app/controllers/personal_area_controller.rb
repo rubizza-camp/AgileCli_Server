949bec8b1c5dc404222167a2163dba547e9e13a9
Andrew Berendeev <soligorsk162@gmail.com>
2019-08-30 12:57:23 +0300

    Add rescue not logined user

--- app/controllers/personal_area_controller.rb
+++ app/controllers/personal_area_controller.rb
@@ -1,6 +1,10 @@
 class PersonalAreaController < ApplicationController
   def cabinet
-    @current_user = User.find(session[:user_id])
-    @projects = @current_user.projects
+    if session[:user_id] == nil
+      redirect_to '/'
+    else
+      @current_user = User.find(session[:user_id])
+      @projects = @current_user.projects
+    end
   end
 end
